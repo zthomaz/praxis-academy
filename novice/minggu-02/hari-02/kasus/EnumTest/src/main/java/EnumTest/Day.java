b36ec11a02ea2e36bee8757fb4ea9bd3ce7466b8
zthomaz <error6119@gmail.com>
2019-10-30 18:07:23 +0700

    2-2

---
+++ novice/minggu-02/hari-02/kasus/EnumTest/src/main/java/EnumTest/Day.java
@@ -0,0 +1,5 @@
+package EnumTest;
+
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
---
+++ novice/minggu-02/hari-02/latihan/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public int multiply(int x, int y) {
+        // the following is just an example
+        if (x > 999) {
+            throw new IllegalArgumentException("X should be less than 1000");
+        }
+        return x / y;
+    }
+}
